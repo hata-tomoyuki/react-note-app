a6f8069a63bb2a7a9a934b5dfd01cb59c6453ea4
hata-tomoyuki <h.tomo0602@gmail.com>
2022-11-16 18:52:50 +0900

    add create function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 import Main from './components/Main'
 import Sidebar from './components/Sidebar'
+import uuid from 'react-uuid'
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "新しいノートの内容です",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+  };
 
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} notes={notes} />
       <Main />
     </div>
   )
