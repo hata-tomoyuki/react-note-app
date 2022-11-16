2c5029d0ccb1d4c8740524372a0ac2d42e056da2
hata-tomoyuki <h.tomo0602@gmail.com>
2022-11-16 18:59:16 +0900

    add delete function

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,14 @@ function App() {
     setNotes([...notes, newNote]);
   };
 
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  }
+
   return (
     <div className="App">
-      <Sidebar onAddNote={onAddNote} notes={notes} />
+      <Sidebar onAddNote={onAddNote} notes={notes} onDeleteNote={onDeleteNote} />
       <Main />
     </div>
   )
